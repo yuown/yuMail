65821632a4fd79c5cfee034868c5d8467f47b9ea
Kiran N K <kirannk>
2016-03-15 16:10:48 +0530

    First Commit

---
+++ src/main/java/yuown/bulk/YuMailApplication.java
@@ -0,0 +1,12 @@
+package yuown.bulk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuMailApplication.class, args);
+	}
+}
